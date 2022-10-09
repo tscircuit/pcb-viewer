f5c8acc76b757293711fd522db3131a15124a551
seveibar <seveibar@gmail.com>
2022-10-08 19:29:26 -0700

    checkpoint basic pad

--- src/pages/viewer.tsx
+++ src/pages/viewer.tsx
@@ -1,11 +1,42 @@
+import { GroupBuilder } from "@tscircuit/builder"
 import React, { useEffect, useRef } from "react"
 import { PCBViewer } from "../PCBViewer"
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      custom: {
+        onRender: (gb: GroupBuilder) => any
+      }
+    }
+  }
+}
 
 export default () => {
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer>
-        <resistor name="R1" />
+        <custom
+          onRender={(gb) => {
+            // gb.addResistor((rb) => {})
+            gb.addComponent((cb) => {
+              cb.footprint.addPad((pb) =>
+                pb
+                  .setShape("rect")
+                  .setLayer("top")
+                  .setPosition(100, 100)
+                  .setSize(20, 20)
+              )
+              cb.footprint.addPad((pb) =>
+                pb
+                  .setShape("rect")
+                  .setLayer("top")
+                  .setPosition(140, 100)
+                  .setSize(20, 20)
+              )
+            })
+          }}
+        />
       </PCBViewer>
     </div>
   )
