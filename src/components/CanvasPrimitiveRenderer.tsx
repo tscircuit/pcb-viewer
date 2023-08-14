b2f975d8dd6ea5443dff0138c88758b4920b315e
Severin Ibarluzea <seveibar@gmail.com>
2023-08-13 22:51:47 -0700

    fix: add supergrid instead of manual grid

--- src/components/CanvasPrimitiveRenderer.tsx
+++ src/components/CanvasPrimitiveRenderer.tsx
@@ -1,4 +1,5 @@
 import { drawGrid } from "lib/draw-grid"
+import { SuperGrid, toMMSI } from "react-supergrid"
 import React, { useEffect, useRef } from "react"
 import { Matrix } from "transformation-matrix"
 import { drawPrimitives } from "../lib/draw-primitives"
@@ -23,10 +24,11 @@ export const CanvasPrimitiveRenderer = ({
 }: Props) => {
   const ref = useRef()
   useEffect(() => {
+    if (!ref.current) return
     const drawer = new Drawer(ref.current)
     if (transform) drawer.transform = transform
     drawer.clear()
-    if (grid) drawGrid(drawer, grid)
+    // if (grid) drawGrid(drawer, grid)
     drawPrimitives(drawer, primitives)
   }, [primitives, transform])
 
@@ -37,9 +39,25 @@ export const CanvasPrimitiveRenderer = ({
         width,
         height,
         border: "1px solid #f00",
+        position: "relative",
       }}
     >
-      <canvas ref={ref} width={width} height={height}></canvas>
+      <SuperGrid
+        textColor="rgba(0,255,0,0.8)"
+        majorColor="rgba(0,255,0,0.4)"
+        minorColor="rgba(0,255,0,0.2)"
+        screenSpaceCellSize={100}
+        width={width}
+        height={height}
+        transform={transform}
+        stringifyCoord={(x, y, z) => `${toMMSI(x, z)}, ${toMMSI(y, z)}`}
+      />
+      <canvas
+        ref={ref}
+        style={{ position: "absolute", left: 0, top: 0, pointerEvents: "none" }}
+        width={width}
+        height={height}
+      ></canvas>
     </div>
   )
 }
