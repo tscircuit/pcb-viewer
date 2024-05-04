242d31ddab01396dd75c20359f342833b196abe8
seveibar <seveibar@gmail.com>
2024-05-03 17:27:50 -0700

    fix: additional exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
 export * from "./PCBViewer"
+export type { EditEvent } from "lib/edit-events"
+export { applyEditEvents } from "lib/apply-edit-events"
 export { CanvasElementsRenderer } from "./components/CanvasElementsRenderer"
