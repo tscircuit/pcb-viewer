06e7d6224472c0d32d468f1facce65c93f2f66fa
seveibar <seveibar@gmail.com>
2024-05-12 22:05:09 -0700

    add componetn with silkscreentext

--- src/stories/silkscreen.stories.tsx
+++ src/stories/silkscreen.stories.tsx
@@ -6,7 +6,21 @@ export const SilkscreenExample: React.FC = () => {
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer>
-        <resistor name="R1" footprint="0805" resistance="10k" />
+        {/* <resistor name="R1" footprint="0805" resistance="10k" /> */}
+        <component
+          name="R1"
+          footprint={
+            <footprint>
+              <silkscreentext
+                text="hello world"
+                pcbX={0}
+                pcbY={0}
+                pcbRotation={0}
+                layer="top"
+              />
+            </footprint>
+          }
+        ></component>
       </PCBViewer>
     </div>
   )
