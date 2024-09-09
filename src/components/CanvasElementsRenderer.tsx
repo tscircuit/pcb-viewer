35cba463cd016f13de3d410d460f9c7041fefcb5
seveibar <seveibar@gmail.com>
2024-09-08 21:05:28 -0700

    Hover Highlights for Elements, add is_mouse_over to primitives

--- src/components/CanvasElementsRenderer.tsx
+++ src/components/CanvasElementsRenderer.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useState } from "react"
 import { CanvasPrimitiveRenderer } from "./CanvasPrimitiveRenderer"
 import type { AnySoupElement } from "@tscircuit/soup"
 import { useMemo } from "react"
 import { convertElementToPrimitives } from "../lib/convert-element-to-primitive"
 import { Matrix } from "transformation-matrix"
-import { GridConfig } from "lib/types"
+import { GridConfig, Primitive } from "lib/types"
 import { MouseElementTracker } from "./MouseElementTracker"
 import { DimensionOverlay } from "./DimensionOverlay"
 import { ToolbarOverlay } from "./ToolbarOverlay"
@@ -28,14 +28,42 @@ export interface CanvasElementsRendererProps {
 
 export const CanvasElementsRenderer = (props: CanvasElementsRendererProps) => {
   const { transform, elements } = props
-  const primitives = useMemo(() => {
-    const primitives = props.elements.flatMap((elm) =>
+  const primitivesWithoutInteractionMetadata = useMemo(() => {
+    const primitivesWithoutInteractionMetadata = props.elements.flatMap((elm) =>
       convertElementToPrimitives(elm, props.elements),
     )
-    return primitives
+    return primitivesWithoutInteractionMetadata
   }, [props.elements])
+
+  const [primitives, setPrimitives] = useState<Primitive[]>(
+    primitivesWithoutInteractionMetadata,
+  )
+
   return (
-    <MouseElementTracker transform={transform} primitives={primitives}>
+    <MouseElementTracker
+      transform={transform}
+      primitives={primitivesWithoutInteractionMetadata}
+      onMouseHoverOverPrimitives={(primitivesHoveredOver) => {
+        const primitiveIdsWithMouseOver = new Set(
+          primitivesHoveredOver.map((p) => p._pcb_drawing_object_id),
+        )
+        const newPrimitives = []
+        for (const primitive of primitivesWithoutInteractionMetadata) {
+          const newPrimitive = { ...primitive }
+          if (primitiveIdsWithMouseOver.has(primitive._pcb_drawing_object_id)) {
+            newPrimitive.is_mouse_over = true
+          } else {
+            newPrimitive.is_mouse_over = false
+          }
+          newPrimitives.push(newPrimitive)
+        }
+
+        // Compute which primitives are connected via the same net
+        // to do this, use a netlist created above
+
+        setPrimitives(newPrimitives)
+      }}
+    >
       <EditPlacementOverlay
         disabled={!props.allowEditing}
         transform={transform}
