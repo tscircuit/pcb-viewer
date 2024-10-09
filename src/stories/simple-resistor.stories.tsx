944bf810546bbdddeabde4aa3f64860ea2edc124
Severin Ibarluzea <seveibar@gmail.com>
2024-10-08 17:04:09 -0700

    Revert "Convert children rendering to core (#70)" (#71)
    This reverts commit 36716288b3afd22a9f0ca392b2bf24bb843e8846.

--- src/stories/simple-resistor.stories.tsx
+++ src/stories/simple-resistor.stories.tsx
@@ -5,9 +5,7 @@ export const SimpleResistorSMD = () => {
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer>
-        <board width="10mm" height="10mm">
-          <resistor name="R1" footprint="0805" resistance="10k" />
-        </board>
+        <resistor name="R1" footprint="0805" resistance="10k" />
       </PCBViewer>
     </div>
   )
@@ -17,9 +15,12 @@ export const SimpleResistorThruHole = () => {
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer>
-        <board width="10mm" height="10mm">
-          <resistor name="R1" footprint="axial" resistance="10k" />
-        </board>
+        {/* <resistor footprint="dip" resistance="10k" /> */}
+        {/* TODO use resistor with some kind of through hole footprint name */}
+        <component name="R1">
+          <platedhole shape="circle" pcbX={0} pcbY={0} holeDiameter="1mm" outerDiameter="2mm" />
+          <platedhole shape="circle" pcbX={6} pcbY={0} holeDiameter="1mm" outerDiameter="2mm" />
+        </component>
       </PCBViewer>
     </div>
   )
@@ -29,15 +30,13 @@ export const SimpleResistorsOffCenter = () => {
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer>
-        <board width="10mm" height="10mm">
-          <resistor
-            name="R1"
-            footprint="0805"
-            resistance="10k"
-            pcbX={5}
-            pcbY={5}
-          />
-        </board>
+        <resistor
+          name="R1"
+          footprint="0805"
+          resistance="10k"
+          pcbX={5}
+          pcbY={5}
+        />
         {/* <resistor
           name="R2"
           footprint="0805"
