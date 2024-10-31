041be8df743cc4c774008026ed526ed49fcf6858
seveibar <seveibar@gmail.com>
2024-10-31 11:40:59 -0700

    standardize zIndexMap

---
+++ src/lib/util/z-index-map.ts
@@ -0,0 +1,9 @@
+export const zIndexMap = {
+  elementOverlay: 30,
+  dimensionOverlay: 30,
+  editTraceHintOverlay: 30,
+  errorOverlay: 30,
+  ratsNestOverlay: 20,
+  toolbarOverlay: 20,
+  topLayer: 10, // each layer after this is 1 less than the previous
+}
