df488cde6715b728213a6a7e70daf048dfa03711
Your Name <you@example.com>
2025-01-06 22:22:05 +0200

    improved PR

--- src/lib/filter-traces-if-multiple.ts
+++ src/lib/filter-traces-if-multiple.ts
@@ -1,11 +1,10 @@
 import { HighlightedPrimitive } from "../components/MouseElementTracker"
 
-export function filterTracesIfMultiple(filterTraces:{
-  primitives: HighlightedPrimitive[],
+export function filterTracesIfMultiple(filterTraces: {
+  primitives: HighlightedPrimitive[]
   is_showing_multiple_traces_length: boolean
-}
-): HighlightedPrimitive[] {
-  const { primitives, is_showing_multiple_traces_length: DISPLAY_ALL_TRACE_LENGTHS } = filterTraces
+}): HighlightedPrimitive[] {
+  const { primitives, is_showing_multiple_traces_length } = filterTraces
   // Filter traces to get only the shortest one
   const traces = primitives.filter(
     (
@@ -23,7 +22,7 @@ export function filterTracesIfMultiple(filterTraces:{
     //  to choose if we want to display multiple traces when we hover on them
 
     // Ignore all traces if we hover on multiple traces
-    if (!DISPLAY_ALL_TRACE_LENGTHS)
+    if (!is_showing_multiple_traces_length)
       return primitives.filter((p) => p._element.type !== "pcb_trace")
 
     const shortestTrace = traces.reduce((shortest, current) => {
