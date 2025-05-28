152cb751748924a69a533d408aac13c9a59c6875
Shibo <shibosoftwaredev@gmail.com>
2025-05-28 18:11:38 +0200

    support nine point anchor alignment for silkscreen texts

--- src/stories/silkscreen/silkscreen-text-alphabet.stories.tsx
+++ src/stories/silkscreen/silkscreen-text-alphabet.stories.tsx
@@ -32,6 +32,95 @@ export const SilkscreenTextAlphabet: React.FC = () => {
   )
 }
 
+export const SilkscreenTextFontSize = () => {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <PCBViewer
+        circuitJson={[
+          {
+            type: "pcb_board",
+            center: { x: 0, y: 0 },
+            width: 10,
+            height: 10,
+            subcircuit_id: "pcb_generic_component_0",
+            material: "fr4",
+            num_layers: 2,
+            pcb_board_id: "pcb_board_0",
+            thickness: 1,
+            is_subcircuit: false,
+          },
+          {
+            type: "pcb_silkscreen_text",
+            layer: "top",
+            pcb_silkscreen_text_id: "pcb_silkscreen_text_0",
+            font: "tscircuit2024",
+            font_size: 1,
+            pcb_component_id: "pcb_generic_component_0",
+            anchor_position: { x: 0, y: 0 },
+            anchor_alignment: "bottom_center",
+            text: "FONT SIZE TEST",
+          },
+          {
+            type: "pcb_silkscreen_text",
+            layer: "top",
+            pcb_silkscreen_text_id: "pcb_silkscreen_text_0",
+            font: "tscircuit2024",
+            font_size: 0.5,
+            pcb_component_id: "pcb_generic_component_0",
+            anchor_position: { x: 0, y: 2 },
+            anchor_alignment: "bottom_center",
+            text: "FONT SIZE TEST",
+          },
+          {
+            type: "pcb_smtpad",
+            layer: "top",
+            pcb_smtpad_id: "pcb_smtpad_0",
+            pcb_component_id: "pcb_generic_component_0",
+            width: 1,
+            height: 1,
+            x: 4.55,
+            y: 0.5,
+            shape: "rect",
+          },
+          {
+            type: "pcb_smtpad",
+            layer: "top",
+            pcb_smtpad_id: "pcb_smtpad_0",
+            pcb_component_id: "pcb_generic_component_0",
+            width: 1,
+            height: 1,
+            x: -4.55,
+            y: 0.5,
+            shape: "rect",
+          },
+          {
+            type: "pcb_smtpad",
+            layer: "top",
+            pcb_smtpad_id: "pcb_smtpad_0",
+            pcb_component_id: "pcb_generic_component_0",
+            width: 1,
+            height: 1,
+            x: -2.55,
+            y: 2.5,
+            shape: "rect",
+          },
+          {
+            type: "pcb_smtpad",
+            layer: "top",
+            pcb_smtpad_id: "pcb_smtpad_0",
+            pcb_component_id: "pcb_generic_component_0",
+            width: 1,
+            height: 1,
+            x: 2.55,
+            y: 2.5,
+            shape: "rect",
+          },
+        ]}
+      />
+    </div>
+  )
+}
+
 const meta: Meta<typeof SilkscreenTextAlphabet> = {
   title: "Silkscreen",
   component: SilkscreenTextAlphabet,
