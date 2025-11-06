39273b466028b87a85826022bba092278d531d0f
seveibar <seveibar@gmail.com>
2025-11-06 09:06:04 -0800

    Board Size Key reset transform

--- src/PCBViewer.tsx
+++ src/PCBViewer.tsx
@@ -13,6 +13,7 @@ import { CanvasElementsRenderer } from "./components/CanvasElementsRenderer"
 import type { ManualEditEvent } from "@tscircuit/props"
 import { zIndexMap } from "lib/util/z-index-map"
 import { calculateCircuitJsonKey } from "lib/calculate-circuit-json-key"
+import { calculateBoardSizeKey } from "lib/calculate-board-size-key"
 
 const defaultTransform = compose(translate(400, 300), scale(40, -40))
 
@@ -65,6 +66,7 @@ export const PCBViewer = ({
     () => calculateCircuitJsonKey(circuitJson),
     [circuitJson],
   )
+  const boardSizeKey = calculateBoardSizeKey(circuitJson)
 
   const resetTransform = () => {
     const elmBounds =
@@ -92,6 +94,12 @@ export const PCBViewer = ({
     setTransform(targetTransform)
     return
   }
+
+  useEffect(() => {
+    if (initialRenderCompleted.current === true) {
+      resetTransform()
+    }
+  }, [boardSizeKey])
 
   useEffect(() => {
     if (!refDimensions?.width) return
