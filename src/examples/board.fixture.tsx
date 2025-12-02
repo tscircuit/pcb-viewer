403939899f54564e1c09aa5ca851d810e086130a
sajalchandhoke <chandhokesajal3@gmail.com>
2025-12-03 02:46:04 +0530

    Fix : /claim #163

--- src/examples/board.fixture.tsx
+++ src/examples/board.fixture.tsx
@@ -1,22 +1,22 @@
-import { Circuit } from "@tscircuit/core"
-import { PCBViewer } from "../PCBViewer"
+import { Circuit } from "@tscircuit/core";
+import { PCBViewer } from "../PCBViewer";
 
 export const BasicRectangleBoard = () => {
-  const circuit = new Circuit()
+  const circuit = new Circuit();
 
-  circuit.add(<board pcbX={0} pcbY={0} width="50mm" height="50mm" />)
+  circuit.add(<board pcbX={0} pcbY={0} width="50mm" height="50mm" />);
 
-  const soup = circuit.getCircuitJson()
+  const soup = circuit.getCircuitJson();
 
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer circuitJson={soup as any} />
     </div>
-  )
-}
+  );
+};
 
 export const TriangleBoard = () => {
-  const circuit = new Circuit()
+  const circuit = new Circuit();
 
   circuit.add(
     <board
@@ -27,22 +27,22 @@ export const TriangleBoard = () => {
       outline={[
         { x: -25, y: 0 },
         { x: 25, y: 0 },
-        { x: 0, y: 25 },
+        { x: 0, y: 25 }
       ]}
-    />,
-  )
+    />
+  );
 
-  const soup = circuit.getCircuitJson()
+  const soup = circuit.getCircuitJson();
 
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer circuitJson={soup as any} />
     </div>
-  )
-}
+  );
+};
 
 export const OctagonBoard = () => {
-  const circuit = new Circuit()
+  const circuit = new Circuit();
 
   circuit.add(
     <board
@@ -58,22 +58,22 @@ export const OctagonBoard = () => {
         { x: -8.28, y: -20 },
         { x: -20, y: -8.28 },
         { x: -20, y: 8.28 },
-        { x: -8.28, y: 20 },
+        { x: -8.28, y: 20 }
       ]}
-    />,
-  )
+    />
+  );
 
-  const soup = circuit.getCircuitJson()
+  const soup = circuit.getCircuitJson();
 
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer circuitJson={soup as any} />
     </div>
-  )
-}
+  );
+};
 
 export const AtariBoard = () => {
-  const circuit = new Circuit()
+  const circuit = new Circuit();
 
   circuit.add(
     <board
@@ -98,37 +98,23 @@ export const AtariBoard = () => {
         { x: -20.5, y: 12.5 },
         { x: -20.5, y: 16.5 },
         { x: -22.5, y: 16.5 },
-        { x: -22.5, y: 24.5 },
+        { x: -22.5, y: 24.5 }
       ]}
-    />,
-  )
+    />
+  );
 
-  const soup = circuit.getCircuitJson()
-
-  return (
-    <div style={{ backgroundColor: "black" }}>
-      <PCBViewer circuitJson={soup as any} />
-    </div>
-  )
-}
-
-export const DisabledAutoFocus = () => {
-  const circuit = new Circuit()
-
-  circuit.add(<board pcbX={0} pcbY={0} width="50mm" height="50mm" />)
-
-  const soup = circuit.getCircuitJson()
+  const soup = circuit.getCircuitJson();
 
   return (
     <div style={{ backgroundColor: "black" }}>
       <PCBViewer circuitJson={soup as any} />
     </div>
-  )
-}
+  );
+};
 
 export default {
   BasicRectangleBoard,
   TriangleBoard,
   OctagonBoard,
-  AtariBoard,
-}
+  AtariBoard
+};
