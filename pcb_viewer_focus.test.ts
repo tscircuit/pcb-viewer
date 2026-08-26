03db5ff7ed90f8fca3848926d597d2bc9be5f8b2
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-26 15:36:24 -0300

    test(viewer): import Bun test globals

--- pcb_viewer_focus.test.ts
+++ pcb_viewer_focus.test.ts
@@ -1,15 +1,21 @@
-import { resolveViewerFocusConfig } from './pcb_viewer_focus_patch';
+import { describe, expect, it } from "bun:test"
 
-describe('PCB Viewer Focus Config', () => {
-  it('should default focusOnHover to true', () => {
-    expect(resolveViewerFocusConfig({})).toEqual({ focusOnHover: true });
-  });
+import { resolveViewerFocusConfig } from "./pcb_viewer_focus_patch"
 
-  it('should respect modern focusOnHover prop', () => {
-    expect(resolveViewerFocusConfig({ focusOnHover: false })).toEqual({ focusOnHover: false });
-  });
+describe("PCB Viewer Focus Config", () => {
+  it("should default focusOnHover to true", () => {
+    expect(resolveViewerFocusConfig({})).toEqual({ focusOnHover: true })
+  })
 
-  it('should map legacy disableAutoFocus prop', () => {
-    expect(resolveViewerFocusConfig({ disableAutoFocus: true })).toEqual({ focusOnHover: false });
-  });
-});
+  it("should respect modern focusOnHover prop", () => {
+    expect(resolveViewerFocusConfig({ focusOnHover: false })).toEqual({
+      focusOnHover: false,
+    })
+  })
+
+  it("should map legacy disableAutoFocus prop", () => {
+    expect(resolveViewerFocusConfig({ disableAutoFocus: true })).toEqual({
+      focusOnHover: false,
+    })
+  })
+})
